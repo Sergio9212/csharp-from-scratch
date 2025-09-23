d464162286e6a4f9fff3e4ad3dea5965fa17ab45
Sergio9212 <S.arboleda1992>
2025-09-23 03:22:05 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld{
+    class Program{
+        static void Main(){
+            string Name = "John";
+            Name = "1John";
+            Console.WriteLine($"Hello World!, {Name}!");
+            Console.WriteLine();
+        }
+    }
+}
