f1a9887d1317d359e4c095da8c358f3f50946cd6
Sergio9212 <S.arboleda1992>
2025-09-23 17:34:48 -0500

    Add conditionals and operators

---
+++ HelloWorld/conditionals_and_operators/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Conditionals*/
+//  Console.WriteLine("Ingrese un numero: ");
+//  int num = int.Parse (Console.ReadLine());
+//  if (num > 0) Console.WriteLine("Positivo");
+//  else if (num < 0) Console.WriteLine("Negativo");
+//  else Console.WriteLine("Es cero");
+
+/*End conditionals*/
+
+/*Operators*/
+Console.WriteLine("Ingrese el primer numero: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("ingrese el segundo numero: ");
+double b = double.Parse(Console.ReadLine());
+const string indeterminado = "Indeterminado";
+Console.WriteLine($"Suma: {a+b}, Resta: {a-b}, Multiplicacion: {a*b}, Division: {(b != 0 ? a/b: indeterminado)}");
+
+/*End operators*/
